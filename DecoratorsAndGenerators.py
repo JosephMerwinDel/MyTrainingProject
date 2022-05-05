126d07b3367ec0d65caed64593e60b0e6f80e8c7
JosephMerwinDel <104882489+JosephMerwinDel@users.noreply.github.com>
2022-05-05 18:47:09 +0530

    Add files via upload

---
+++ DecoratorsAndGenerators.py
@@ -0,0 +1,26 @@
+def fibinocci_sequence(n):
+    a = 1
+    b= 1
+    for i in range(n):
+        yield a
+        a,b = b, a+b
+
+number = int(input("Enter input: "))
+for nmber in fibinocci_sequence(number):
+    print(nmber)
+
+
+def executetwice(multiply):
+	def inner(num1, num2):
+		multiply(num1, num2)
+		multiply(num1, num2)
+	return inner
+
+@executetwice
+def multiply(num1, num2):
+	print("Decorator: \n", num1 * num2)
+
+multiply(4,4)
+
+
+
