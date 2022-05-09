9bb2f54456383e79bc225ad11363209804cf1fee
JosephMerwinDel <104882489+JosephMerwinDel@users.noreply.github.com>
2022-05-09 21:13:52 +0530

    Add files via upload

---
+++ OpenBrowser.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+#driver = webdriver.Chrome(executable_path="C:\Automation")
+#driver.get("https://www.google.com/")
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.maximize_window()
+driver.get("https://www.google.com")
+
+#driver.find_element_by_xpath("//input[@class = 'gLFyf gsfi']")
+driver.find_element(by=By.XPATH, value="//input[@class = 'gLFyf gsfi']").send_keys("outlook")
+driver.implicitly_wait(0.8)
+driver.find_element(by=By.XPATH, value="//div[@class = 'mkHrUc' ]//span[text() = 'outlook']//b[text()=' 365']").click()
